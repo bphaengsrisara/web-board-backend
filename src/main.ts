1089045e6567ea089783c876997000c944e44120
Bovonrajt Phaengsrisara <bovonrajt.p@gmail.com>
2024-12-02 00:32:02 +0700

    refactor(env): move port to .env

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(8000);
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
